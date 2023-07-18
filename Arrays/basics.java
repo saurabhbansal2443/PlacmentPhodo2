dfd42f47a515b66726b2055800532c8a9376c2b4
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-18 14:03:25 +0530

    array Basics

---
+++ Arrays/basics.java
@@ -0,0 +1,49 @@
+package Arrays;
+
+public class basics {
+public static void main(String[] args) {
+      
+       // DataType[] arrayName = new DataType[Size];
+
+
+       //1 Arrays Declartion 
+
+      // int[] arr;
+
+       //2  intializtion 
+
+       //arr = new int[5];
+
+       // length of an array 
+
+       //int l = arr.length;
+
+       //System.out.println(l);
+
+
+       // insert an element in array 
+
+    //    arr[0]=99;
+    //    arr[1]=56;
+    //    arr[2]=100;
+    //    arr[3]=78;
+    //    arr[4]=34;
+       //arr[5]=89; this will give error 
+
+       // display an array 
+
+       int[] arr = new int[4];
+
+       for(int i=0;i<arr.length;i++){
+           System.out.println(arr[i]);
+    
+       }
+
+    //    int[] arr = {1,2,3,4,45,7,5,4,2,4,9};
+
+    //    System.out.println(arr.length-1);
+
+
+    }
+
+}
