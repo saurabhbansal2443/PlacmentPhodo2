4d94ced724841bb37d9a91ad1e9c79cec0f83797
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-20 15:02:58 +0530

    add two number

---
+++ Arrays/AddTwoArray.java
@@ -0,0 +1,51 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class AddTwoArray {
+    public static void main(String[] args) {
+        int[] arr1 = { 9, 9, 1, 3, 7, 6, 5 };
+        int[] arr2 = { 8, 9, 3, 4 };
+
+        int resLength = Math.max(arr1.length, arr2.length);
+
+        int[] res = new int[resLength];
+
+        int p1 = arr1.length - 1;
+        int p2 = arr2.length - 1;
+        int p3 = res.length - 1;
+
+        int carry = 0;
+
+        while (p1 >= 0 || p2 >= 0) {
+
+            int val1 = p1 < 0 ? 0 : arr1[p1];
+            int val2 = p2 < 0 ? 0 : arr2[p2];
+
+            int sum = val1 + val2 + carry;
+
+            int d = sum%10;
+            carry = sum/10;
+
+            res[p3]=d;
+
+            p1--;
+            p2--;
+            p3--;
+
+        }
+
+        if(carry>0){
+            int[] res2 = new int[res.length+1];
+            res2[0]=carry;
+
+            for(int i=0;i<res.length;i++){
+                res2[i+1]=res[i];
+            }
+
+            res =res2;
+        }
+
+        System.out.println(Arrays.toString(res));
+    }
+}
--- Arrays/mergeTwoSortedArray.java
+++ Arrays/mergeTwoSortedArray.java
@@ -1,7 +1,47 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class mergeTwoSortedArray {
     public static void main(String[] args) {
-        
+        int[] arr1 = {1,4,6,10,12,18,20,22,23,24};
+        int[] arr2 = {2,3,5,5,6,8,9,10};
+
+        int[] res = new int[arr1.length + arr2.length];
+
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+
+        while(p1<arr1.length && p2<arr2.length){
+
+            if(arr1[p1]<arr2[p2]){
+                res[p3]=arr1[p1];
+                p1++;
+                p3++;
+            }else{
+                res[p3]=arr2[p2];
+                p2++;
+                p3++;
+            }
+        }
+
+        if(p1==arr1.length){
+
+            for(int i=p2;i<arr2.length;i++){
+                res[p3]=arr2[i];
+                p3++;
+            }
+        }else{
+            for(int i=p1;i<arr1.length;i++){
+                res[p3]=arr1[i];
+                p3++;
+            }
+        }
+
+           System.out.println(Arrays.toString(res));
+           
+
+
     }
 }
