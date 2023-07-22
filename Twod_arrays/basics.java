827bd283109c21c235669d4e526bf4b2c1dba618
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-22 13:16:59 +0530

    2d array started

---
+++ Twod_arrays/basics.java
@@ -0,0 +1,12 @@
+package Twod_arrays;
+
+public class basics {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][4];
+
+        int rows = arr.length;
+        int col = arr[0].length;
+
+        System.out.println(rows + " " + col);
+    }
+}
