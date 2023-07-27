4d99b9403d75d68a970e9cab7afafc2178d51fbe
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-27 13:39:01 +0530

    reverse a string

--- Strings/reverseString.java
+++ Strings/reverseString.java
@@ -1,5 +1,20 @@
 public class reverseString{
     public static void main(String[] args) {
-        
+        String str = "RadaR";
+
+        String ans = reverse(str);
+
+        System.out.println(ans);
+    }
+
+    public static String reverse(String str){
+
+        String ans = "";
+
+        for(int i=str.length()-1;i>=0;i--){
+            ans+=str.charAt(i);      
+          }
+
+          return ans ;
     }
 }
